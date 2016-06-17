975dc43caff2c213e40287806d52ee4cadc0fc63
Cathy Meng <cathy.meng@medibank.com.au>
2016-06-17 20:27:56 +1000

    automation on linkedin login - Geb Groovy Chrome

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,16 +1,18 @@
-import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType
 import org.openqa.selenium.remote.DesiredCapabilities
 
-driver = "firefox"
-//reportsDir = "build/reports/geb"
+driver = "chrome"
 baseUrl = "https://www.linkedin.com"
 waiting {
     timeout = 2
 }
 
+//Replace the absolute path pointing to the place where your chromedriver locates
+System.setProperty("webdriver.chrome.driver", new File("C:/workspace/seleniumdrivers/chromedriver.exe").getAbsolutePath())
+
 environments {
-    firefox {
+    chrome {
         driver = {
             caps = new DesiredCapabilities();
 
@@ -29,7 +31,33 @@ environments {
                 caps.setCapability(CapabilityType.PROXY, proxy);
 
             }
-            new FirefoxDriver(caps)
+            new ChromeDriver(caps)
         }
     }
 }
+
+//Use the code below for testing on Firefox browser
+//environments {
+//    firefox {
+//        driver = {
+//            caps = new DesiredCapabilities();
+//
+//            caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+//
+//            if (System.props.'http.proxyHost') {
+//
+//                String PROXY = System.props.'http.proxyHost' + ":" + System.props.'http.proxyPort'
+//
+//                proxy = new org.openqa.selenium.Proxy();
+//                proxy.setHttpProxy(PROXY)
+//                        .setFtpProxy(PROXY)
+//                        .setSslProxy(PROXY)
+//                        .setNoProxy("127.0.0.1, *.local, localhost");
+//
+//                caps.setCapability(CapabilityType.PROXY, proxy);
+//
+//            }
+//            new FirefoxDriver(caps)
+//        }
+//    }
+//}
